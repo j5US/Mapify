6bccad501896682eb1f60311f73454b040298863
Mriganka Paul <ironmaniscool99@gmail.com>
2024-12-25 00:32:49 +0530

    fixed map marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,20 +3,28 @@ import { Map as LeafletMap } from "leaflet";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useRef } from "react";
+import L from "leaflet";
+import pin from "./pin.png";
 
 interface MapProps {
     place: Place | null;
 }
 
 export default function Map({ place }: MapProps) {
+    const customIcon = L.icon({
+        iconUrl: pin,
+        iconSize: [28, 38], 
+        iconAnchor: [16, 42], 
+        // popupAnchor: [0, -32], 
+    });
 
     const mapRef = useRef<LeafletMap | null>(null);
 
-    useEffect(()=>{
-        if( mapRef.current && place){
+    useEffect(() => {
+        if (mapRef.current && place) {
             mapRef.current.flyTo([place.latitude, place.longitude]);
         }
-    },[place]);
+    }, [place]);
 
     return <MapContainer
         ref={mapRef}
@@ -25,7 +33,7 @@ export default function Map({ place }: MapProps) {
         scrollWheelZoom
         className="h-full"
     >
-        <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-        {place && <Marker position={[place.latitude, place.longitude]}/>}
+        <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {place && <Marker position={[place.latitude, place.longitude]} icon={customIcon}/>}
     </MapContainer>;
 }
